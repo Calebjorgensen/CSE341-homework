5f2fe90489569a4ced03f711632377afcc38954d
Calebjorgensen <121927129+Calebjorgensen@users.noreply.github.com>
2025-01-24 20:15:59 -0800

    Folders added

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const calebRoute = (req, res) => {
+    res.send('Caleb Jorgensen');
+};
+
+const elleRoute = (req, res) => {
+    res.send('Elle Jorgensen');
+};
+
+module.exports = {
+    calebRoute,
+    elleRoute,
+};
